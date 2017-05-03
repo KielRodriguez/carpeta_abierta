59fc2c2d0edb8e95249b404f433c90a58192c9f2
Giovanni Mendoza <mendozagioo@gmail.com>
2017-05-03 01:51:30 -0500

    Generación de un nuevo json para mostrar los cambios específicos para cada carpeta que cambió

--- recibidor/generator.js
+++ recibidor/generator.js
@@ -48,7 +48,9 @@
           ids.push(pureDatas[1]);
           
           // Agrega un elemento no existente aún
+          // inde --> es el índice del json que contiene los datos completos
           jsonResult.push({
+            "inde": i,
             "id": pureDatas[1],
             "date": pureDatas[0].substring(0, 10).trim(),
             "date_start": pureDatas[14],
@@ -62,8 +64,16 @@
             "value": 1
           });
         } else {
+          // Obtiene el índice del json que contiene el mismo id
+          // que el json generado(jsonResult)
+          var index_json = jsonResult[indexId].inde;
+          // Se obtienen los datos de una carpeta anterior con el mismo id
+          var datas_found = datas[index_json].replace(/"/g,"").split(',');
+
           // Reemplazar elemento y contar un cambio
+          // inde --> es el índice del json que contiene los datos completos
           jsonResult[indexId] = {
+            "inde": i,
             "id": pureDatas[1],
             "date": pureDatas[0].substring(0, 10).trim(),
             "date_start": pureDatas[14],
@@ -74,8 +84,22 @@
             "verdict": veredict,
             "state": state,
             "changes": jsonResult[indexId].changes + 1,
+            "complete_changes": [],
             "value": 1
           };
+
+          // Agregar elementos que no son iguales
+          // ya que estos son cambios en la carpeta
+          for (var j = 0; j <= pureDatas.length - 1; j++) {
+            // Si los datos no son iguales, entonces...
+            // ése es uno de los cambios que se deben agregar
+            if (pureDatas[j] !== datas_found[j]) {
+              jsonResult[indexId].complete_changes.push({
+                "title_text": titles[j],
+                "value": pureDatas[j]
+              });
+            }
+          }
         }
       }
 
