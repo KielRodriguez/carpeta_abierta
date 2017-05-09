c85f0ca1ba6d1dba9cf045e9e4a62d49454e487b
Giovanni Mendoza <mendozagioo@gmail.com>
2017-05-09 00:39:44 -0500

    WIP para generar el mapa coroplético con sus respectivos cambios (en éste punto sólo se muestra el mapa de México con sus municipios y sin datos)

--- map/generator.js
+++ map/generator.js
@@ -5,3 +5,56 @@
 // de acuerdo a cada filtro seleccionado en la sección
 // de mapa coroplético
 // ====================================================
+
+(function() {
+  'use strict';
+
+  exports.Generator = {
+    watch: function() {
+      console.log("Im watching you!!");
+
+      // // sample data array
+      // var sample_data = [
+      //   {"value": 7123, "country": "namex", "name": "México"},
+      //   {"value": 1349, "country": "nausa", "name": "Estados Unidos"}
+      // ]
+      // // instantiate d3plus
+      // var visualization = d3plus.viz()
+      //   .container("#map-viz")        // container DIV to hold the visualization
+      //   .data(sample_data)        // data to use with the visualization
+      //   //.coords("http://d3plus.org/topojson/countries.json") // pass topojson coordinates
+      //   .coords({
+      //     "solo": ["namex"],
+      //     "value": "http://d3plus.org/topojson/countries.json"
+      //   })
+      //   .type("geo_map")          // visualization type
+      //   .id("country")            // key for which our data is unique on
+      //   .text("name")             // key to use for display text
+      //   .color("value")           // key for coloring countries
+      //   .tooltip("value")         // keys to place in tooltip
+      //   .draw()                   // finally, draw the visualization!
+      // ;
+
+
+
+      // var svg = d3.select("svg");
+
+      // var path = d3.geoPath();
+
+      // d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
+      //   if (error) throw error;
+
+      //   svg.append("g")
+      //       .attr("class", "counties")
+      //     .selectAll("path")
+      //     .data(topojson.feature(us, us.objects.counties).features)
+      //     .enter().append("path")
+      //       .attr("d", path);
+
+      //   svg.append("path")
+      //       .attr("class", "county-borders")
+      //       .attr("d", path(topojson.mesh(us, us.objects.counties, function(a, b) { return a !== b; })));
+      // });
+    }
+  }
+})();
--- map/selector.js
+++ map/selector.js
@@ -5,3 +5,11 @@
 // usuario (nivel, filtros y año) en la sección
 // de mapa coroplético
 // ====================================================
+
+(function() {
+  'use strict';
+
+  var Generator = require('../map/generator.js').Generator;
+
+  Generator.watch();
+})();
