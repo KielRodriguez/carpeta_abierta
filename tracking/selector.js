41cbbb87c36e9d0c54e4b72003cefc59156865b5
Giovanni Mendoza <mendozagioo@gmail.com>
2017-05-31 01:27:50 -0500

    Se agregó la lógica para agregar los últimos cambios en las carpetas para la viz de tracking

--- tracking/selector.js
+++ tracking/selector.js
@@ -40,4 +40,47 @@
       Generator.searchAndPrintId( $folderId.val().trim(), parseInt($years.val()) );
     }
   });
+
+  // var content = "";
+
+  // titleDatas.forEach(function(val, ind) {
+  //   content += '<div class="card item item-horizontal">' +
+  //                '<h5>'+ val +'</h5>' +
+  //                '<p>'+ ++ind +'</p>' +
+  //              '</div>'
+  //   ;
+  // });
+
+  // $('#horizontal-scroll').append(
+  //   '<h4>Estatus actual de la carpeta</h4>' +
+  //   '<p>25 Mayo 2017</p>' +
+  //   '<div class="content owl-carousel owl-theme">' +
+  //     content +
+  //   '</div>'
+  // );
+
+  // owl.owlCarousel({
+  //   loop: true,
+  //   margin: 10,
+  //   nav: false,
+  //   dots: false,
+  //   responsive: {
+  //     0: {
+  //       items:1
+  //     },
+  //     600: {
+  //       items:3
+  //     },
+  //     1000: {
+  //       items:5
+  //     }
+  //   }
+  // }).trigger('refresh.owl.carousel');
+
+  // $('#tracking .left-click').on('click', function() {
+  //   owl.trigger('prev.owl.carousel');
+  // });
+  // $('#tracking .right-click').on('click', function() {
+  //   owl.trigger('next.owl.carousel');
+  // });
 })();
